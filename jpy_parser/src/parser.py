86d34d9d6d52b03ae1df64ac9dc19e25c4927e13
justy3 <saurabhjnvj@gmail.com>
2024-08-05 16:17:21 +0530

    fix

--- jpy_parser/src/parser.py
+++ jpy_parser/src/parser.py
@@ -395,13 +395,13 @@ class JPY_shares:
 
 if __name__=="__main__":
 	# EXAMPLE
-	mypath = os.getcwd()
-	pdffiles = [f for f in os.listdir(mypath+"/../data/") if f[-1]=="f"]
+	mypath = "/home/kumarsau/private/capula/pyhelp/jpy_parser/data/"
+	pdffiles = [f for f in os.listdir(mypath) if f[-1]=="f"]
 
 	jpy_parsed = []
 	jpy_parsed_df = []
 	for pdf in pdffiles:
-		jpy = JPY_shares("/home/kumarsau/private/capula/pyhelp/jpy_parser/data/" + pdf)
+		jpy = JPY_shares(mypath + pdf)
 		jpy_parsed.append(jpy)
 		jpy_parsed_df.append(jpy.dataframe)
 		# print(jpy.dataframe)
