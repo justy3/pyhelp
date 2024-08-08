fb6c407fb875f1e0d5dab643502a18227768550a
justy3 <saurabhjnvj@gmail.com>
2024-08-08 15:02:39 +0530

    reorganize and fix

--- jpy_parser/src/parser.py
+++ jpy_parser/src/parser.py
@@ -60,8 +60,8 @@ class JPY_shares:
 		self.en_lines = self.en_translated_lower.split("\n")
 
 		# for fixing the space in output from buggy translator module
-		self.lines_comma_replaced = re.sub("(\d) ,(\d)", r"\1,\2", self.en_translated_lower).split("\n")
-		self.lines_comma_replaced = re.sub("(\d), (\d)", r"\1,\2", self.en_translated_lower).split("\n")
+		temp = re.sub("(\d) ,(\d)", r"\1,\2", self.en_translated_lower)
+		self.lines_comma_replaced = re.sub("(\d), (\d)", r"\1,\2", temp).split("\n")
 
 		# populate fields with dummy values
 		self.ticker_id = None
